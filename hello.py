ecba24c9ffb676e80908bf2d66a6a43cf08a58ae
vchrisb <vchrisb@users.noreply.github.com>
2016-04-04 09:50:43 +0200

    update

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 """Cloud Foundry test"""
 from flask import Flask, render_template, Markup
 import os
+import json
 
 app = Flask(__name__)
 
@@ -8,10 +9,13 @@ COLOR = "#33CC33"
 
 # get CF environment variables
 port = int(os.getenv("PORT"))
-instance = int(os.getenv("CF_INSTANCE_INDEX"))
-ip = os.getenv("CF_INSTANCE_IP")
 
-content = '<h1>Hello World!</h1></br>I am running on instance <strong>' + str(instance) + '</strong> serving on IP <strong>' + ip + '</strong>!'
+VCAP_APPLICATION = json.loads(os.environ['VCAP_APPLICATION'])
+app_instance = int(VCAP_APPLICATION['instance_index'])
+app_guid = VCAP_APPLICATION['application_id']
+app_name = VCAP_APPLICATION['application_name']
+
+content = '<h1>Hello World!</h1></br>I am instance <strong>#' + str(app_instance) + '</strong> serving application <strong>' + app_name + '</strong> with GUID <strong> ' + app_guid + ' !'
 content = Markup(content)
 
 @app.route('/')
