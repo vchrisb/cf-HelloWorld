02c09d4f66f23eedce4bd1c087e4ae41a0fd4bcd
vchrisb <vchrisb@users.noreply.github.com>
2018-10-07 23:52:00 +0200

    fix

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,5 @@ set -e -x
 mkdir build-output/vendor
 pip download -d build-output/vendor -r source-code/requirements.txt --no-binary :all:
 cp -a source-code/{hello.py,runtime.txt,manifest.yml,requirements.txt,Procfile,templates} build-output/
+pwd
 ls -l build-output/
