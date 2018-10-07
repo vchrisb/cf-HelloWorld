58f1c1e4424f3e1d1be76f31f7005db5878d74b0
vchrisb <vchrisb@users.noreply.github.com>
2018-10-07 22:12:19 +0200

    fix

--- ci/test.sh
+++ ci/test.sh
@@ -4,11 +4,9 @@ set -e -x
 
 cd source-code
 python -m venv ./.venv
-ls -l
-ls -l .venv/bin
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install nose
 export PORT=8080
-export VCAP_APPLICATION='{"application_name": "cf-helloworld", "instance_index": "0", "application_id": "f618b72c-f42c-4bc5-adc7-2ec0156ac944"}
+export VCAP_APPLICATION='{"application_name": "cf-helloworld", "instance_index": "0", "application_id": "f618b72c-f42c-4bc5-adc7-2ec0156ac944"}''
 nosetests -v
