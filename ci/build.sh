6e12d90cd564eedb05bee1da0e7ab7862fbf19ca
vchrisb <vchrisb@users.noreply.github.com>
2018-10-07 21:52:50 +0200

    add CI

---
+++ ci/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e -x
+
+
+mkdir build-output/vendor
+pip download -d build-output/vendor -r source-code/requirements.txt --no-binary :all:
+cp -a source-code/{hello.py,runtime.txt,manifest.yml,requirements.txt,Procfile,templates}  build-output/
